9948bcbeccca9896386c10ea952845345c8c0842
majid <majidmohsenifar@gmail.com>
2024-10-29 19:03:56 +0330

    btc added using blockbook

--- src/service/blockchain/btc.rs
+++ src/service/blockchain/btc.rs
@@ -2,24 +2,16 @@ use serde::Deserialize;
 
 use super::{error::BlockchainError, service::BlockchainConfig};
 
+const ADDRESS_URI: &str = "api/v2/address";
+
 pub struct BtcHandler {
     cfg: BlockchainConfig,
     http_client: reqwest::Client,
 }
 
 #[derive(Debug, Deserialize)]
-struct BtcNodeResponse {
-    result: f64,
-    error: Option<String>,
-    id: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct BtcNodeRequest {
-    jsonrpc: String,
-    id: String,
-    method: String,
-    params: Vec<String>,
+struct GetAddressResponse {
+    pub balance: String,
 }
 
 impl BtcHandler {
@@ -28,36 +20,38 @@ impl BtcHandler {
     }
 
     pub async fn get_balance(&self, addr: &str) -> Result<f64, BlockchainError> {
-        //TODO: handle this later
-        unimplemented!()
-        //TODO: handle the req body
-        //let req_body = "palang";
-        //let response = self
-        //.http_client
-        //////TODO: what to update here for uri
-        //.post(format!("{}/", self.cfg.url))
-        //.json(req_body)
-        //.send()
-        //.map_err(|e| BlockchainError::Unexpected {
-        //message: "cannot call the btc node".to_string(),
-        //source: Box::new(e) as Box<dyn std::error::Error + Send + Sync>,
-        //})?;
+        let response = self
+            .http_client
+            .get(format!("{}/{}/{}", self.cfg.url, ADDRESS_URI, addr))
+            .send()
+            .await
+            .map_err(|e| BlockchainError::Unexpected {
+                message: "cannot call the btc node".to_string(),
+                source: Box::new(e) as Box<dyn std::error::Error + Send + Sync>,
+            })?;
 
-        //let body = response.bytes().map_err(|e| BlockchainError::Unexpected {
-        //message: "cannot read the response of the btc node".to_string(),
-        //source: Box::new(e) as Box<dyn std::error::Error + Send + Sync>,
-        //})?;
-        //let res: BtcNodeResponse =
-        //serde_json::from_slice(&body).map_err(|e| BlockchainError::Unexpected {
-        //message: "cannot parse the response".to_string(),
-        //source: Box::new(e) as Box<dyn std::error::Error + Send + Sync>,
-        //})?;
+        let body = response
+            .bytes()
+            .await
+            .map_err(|e| BlockchainError::Unexpected {
+                message: "cannot read the response of the btc node".to_string(),
+                source: Box::new(e) as Box<dyn std::error::Error + Send + Sync>,
+            })?;
+        let res: GetAddressResponse =
+            serde_json::from_slice(&body).map_err(|e| BlockchainError::Unexpected {
+                message: "cannot parse the response".to_string(),
+                source: Box::new(e) as Box<dyn std::error::Error + Send + Sync>,
+            })?;
 
-        //Ok(res.result / 10u32.pow(self.cfg.decimals as u32) as f64)
+        let b: f64 = res
+            .balance
+            .parse()
+            .map_err(|e| BlockchainError::Unexpected {
+                message: "cannot parse string balance".to_string(),
+                source: Box::new(e) as Box<dyn std::error::Error + Send + Sync>,
+            })?;
 
-        ////$ curl -u "$bitcoinauth" -d '{"jsonrpc": "1.0", "id": "curltest", "method": "getbalance", "params": ["*", 6]}' -H 'content-type: text/plain;' http://127.0.0.1:18332/wallet/test-wallet
-        ////{"result":0.00000000,"error":null,"id":"curltest"}
-        //Ok(2.0)
+        Ok(b / 10u32.pow(self.cfg.decimals as u32) as f64)
     }
 
     pub async fn get_token_balance(&self, _: &str, _: &str, _: u8) -> Result<f64, BlockchainError> {
